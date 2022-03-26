4bdadd9403ee6bb1046a8ef73049976d19f4b0bf
khadizaliza <khadizaakter19836@gmail.com>
2022-03-26 20:16:18 +0600

    header-section

---
+++ src/components/Header/Header.js
@@ -0,0 +1,23 @@
+
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+         <h1 className='title'>Best Showpiece Collection!!!</h1> 
+         <h4>Choose 4 Items</h4>  
+        </div>
+    );
+};
+
+export default Header;
+
+
+
+
+
+
+
+
+
